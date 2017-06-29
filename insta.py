31073e7c24117642d90c8b9f4e0d8af2f3004aaa
akshat pradhan <akat1296@gmail.com>
2017-06-29 11:29:48 +0530

    Update insta.py

--- insta.py
+++ insta.py
@@ -1,7 +1,7 @@
 import urllib.request,json
 
-client_id ="6e46a9d928914100a8b0ed0659511da5"
-client_secret="c673cf7d18b44f1bac876bc93e0b9cd2"
+client_id ="kek"
+client_secret="kerokero"
 redirect_uri="https://127.0.0.1"
 scope="public_content"
 req_url="https://api.instagram.com/oauth/authorize/?client_id="+client_id+"&redirect_uri="+redirect_uri+"&response_type=token&scope="+scope
