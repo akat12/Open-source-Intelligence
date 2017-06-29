ebc6155ad0916456d822deded3ae44b4560501cf
akshat pradhan <akat1296@gmail.com>
2017-06-29 11:34:05 +0530

    Update twit.py

--- twit.py
+++ twit.py
@@ -5,12 +5,12 @@ with open('config') as json_data:
 
 name = prof['person_1']['name']
 
-api_key ="rID32Pp1AiyINX6saYB3L7Ver"
-api_secret="Fs3tuAz69BvLnVP6faMsXGeb3zETZ4t7d4QNDlMxJUdXKxdIP2"
-access_token="876043429905444864-wLthJ70dFKhHuHtekyn2whGcCleYhum"
-access_secret="k9CDGjN7GfBvWMWbcXGRCcKpGeaPiIn8tvmrcGRWqzJNc"
+api_key ="mynameajeff"
+api_secret="XXXX"
+access_token="XYZ"
+access_secret="psyduck"
 api = twitter.Api(api_key,api_secret,access_token,access_secret)
 data = api.GetUsersSearch(name)
 print(data)
-user = api.GetUser(68225924)
+user = api.GetUser(XXXX) #enter id num here
 print (user)
