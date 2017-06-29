56dd1a1e36a5e3c50ee325a56a6baabc281c617c
akshat pradhan <akat1296@gmail.com>
2017-06-29 11:31:52 +0530

    Update serch.py

--- serch.py
+++ serch.py
@@ -6,9 +6,9 @@ start = time.time()
 list = general.readxsl("employee.xlsx")
 link =general.encoder('http://webmii.com/people?n=\"'+list[2][0]+'\"')
 webmii_search =downloader.download(link,'//div[@class="section"][@id="article"]//*')[0]
-email_gsearch= downloader.google_search(str(list[2][2]),"AIzaSyAK8_nYtI5mV7hYMfAESMp5wk8FIoOc9eU" ,"000597688297972046908:wpcvmbousjm", num=10)
-name_gsearch= downloader.google_search(str(list[2][0]),"AIzaSyAK8_nYtI5mV7hYMfAESMp5wk8FIoOc9eU" ,"000597688297972046908:wpcvmbousjm", num=10)
-pastebin_search=downloader.google_search(str(list[2][3]),"AIzaSyDai2S6pC_VaDCfYsFYqjPk5Qf23_TwAYQ" ,"000597688297972046908:nfsxuyodqhe", num=10)
+email_gsearch= downloader.google_search(str(list[2][2]),"XXX" ,"XXXX", num=10)
+name_gsearch= downloader.google_search(str(list[2][0]),"XXXX" ,"XXXX", num=10)
+pastebin_search=downloader.google_search(str(list[2][3]),"XXXX" ,"XXXXX", num=10)
 res={'a-name': list[2][0],'b-webmii':webmii_search,'c-email_google_search':email_gsearch,'d-name_search':name_gsearch,'e-paste_search':pastebin_search}
 print json.dumps(res,sort_keys=True,indent=4,separators=(',',':'))
 
